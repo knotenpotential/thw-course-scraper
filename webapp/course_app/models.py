f7ebc4b465a46a1f71a0a3ba8b3afd194d9ce0be
DominikSudwischer <dominik.sudwischer@gmail.com>
2019-12-21 20:56:45 +0100

    Updated models
    For automatic timestamps for creation/edit date. Also changed deletion handling to DO_NOTHING.

--- webapp/course_app/models.py
+++ webapp/course_app/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+# Mixin to add automatic creation and edit date fields to our model.
+# Source: https://stackoverflow.com/questions/3429878/automatic-creation-date-for-django-model-form-objects
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
 # Class for storing training blueprints such as
 # https://www.thw-ausbildungszentrum.de/SharedDocs/Lehrgaenge/THW-BuS/DE/LG_1_663.html?nn=6228646
 class TrainingBlueprint(models.Model):
@@ -10,12 +20,15 @@ class TrainingBlueprint(models.Model):
     prerequisites = models.TextField()
     goal = models.TextField()
     content = models.TextField()
+    last_edit_at = models.DateTimeField(auto_now=True)
 
 
 # Class for storing concrete training instances that are derived from the blueprint
 class TrainingDate(models.Model):
-    training = models.ForeignKey(TrainingBlueprint, on_delete=models.CASCADE)  # Should we cascade here?
-    training_id = models.CharField(max_length=20)
+    training = models.ForeignKey(TrainingBlueprint, on_delete=models.DO_NOTHING)  # When a blueprint is deleted (e.g.
+    #  because the training is no longer offered), the historic trainigs should not be deleted
+    training_id = models.CharField(max_length=20, db_index=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     deadline = models.DateField()
+    last_edit_at = models.DateTimeField(auto_now=True)
